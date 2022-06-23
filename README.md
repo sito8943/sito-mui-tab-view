00afef022ee05bb91164ef50b3e68a3847e21616
Sito8943 <sito8943@gmail.com>
2022-06-23 18:21:53 -0400

    ready

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # container
+
 Contenedor súper flexible, muy parecido al Box de @mui, pero por defecto es flex
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import * as ReactDOMClient from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import SitoContainer from "./App";
+
+const container = document.getElementById("root");
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the root.
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <SitoContainer>Hola Mundo</SitoContainer>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
