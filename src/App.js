7d0415f1eb1a7d1c15700563328cc397b5fe5176
Sito8943 <sito8943@gmail.com>
2022-06-23 18:13:21 -0400

    component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,86 @@
-import PropTypes from "prop-types";
+import { forwardRef } from "react";
 
 import { Box } from "@mui/material";
 
-function SitoContainer(props) {
-  const { children, className, sx, style } = props;
+// prop-types
+import PropTypes from "prop-types";
+
+const SitoContainer = forwardRef((props, ref) => {
+  const {
+    background,
+    fullWidth,
+    extraProps,
+    component,
+    children,
+    display,
+    alignItems,
+    justifyContent,
+    flexDirection,
+    className,
+    sx,
+    id,
+    name,
+    style,
+  } = props;
+
+  const newSx = {
+    flexDirection,
+    display,
+    alignItems,
+    justifyContent,
+    width: fullWidth ? "100%" : "initial",
+    background: background || "transparent",
+    ...sx,
+  };
 
   return (
-    <Box id="hola" className={className} sx={sx} style={style}>
+    <Box
+      ref={ref}
+      component={component}
+      style={style}
+      id={id}
+      name={name}
+      sx={newSx}
+      className={className}
+      {...extraProps}
+    >
       {children}
     </Box>
   );
-}
+});
 
 SitoContainer.defaultProps = {
+  component: "div",
+  background: "transparent",
+  display: "flex",
+  alignItems: "left",
+  justifyContent: "left",
+  flexDirection: "row",
   className: "",
+  id: "",
+  name: "",
+  fullWidth: false,
   sx: {},
   style: {},
-  children: <></>,
+  extraProps: {},
+  children: <span>This is a container</span>,
 };
 
 SitoContainer.propTypes = {
   children: PropTypes.node,
+  background: PropTypes.string,
+  display: PropTypes.string,
+  component: PropTypes.string,
+  flexDirection: PropTypes.string,
   className: PropTypes.string,
+  alignItems: PropTypes.string,
+  justifyContent: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  fullWidth: PropTypes.bool,
+  /**
+   * The system prop that allows defining system overrides as well as additional CSS styles.
+   */
   sx: PropTypes.oneOfType([
     PropTypes.arrayOf(
       PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
@@ -30,6 +89,13 @@ SitoContainer.propTypes = {
     PropTypes.object,
   ]),
   style: PropTypes.oneOfType([
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
+    ),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+  extraProps: PropTypes.oneOfType([
     PropTypes.arrayOf(
       PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
     ),
