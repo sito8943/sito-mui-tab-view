44b9c14c83ca4f277d14573dca287330af98f440
Sito8943 <sito8943@gmail.com>
2022-10-21 16:55:00 -0400

    doing up

--- dist/App.js
+++ dist/App.js
@@ -7,31 +7,34 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import { jsxs as _jsxs } from "react/jsx-runtime";
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-  return /*#__PURE__*/ _jsx(Box, {
+  const {
+    children,
+    value,
+    index,
+    ...other
+  } = props;
+  return /*#__PURE__*/_jsx(Box, {
     role: "tabpanel",
     hidden: value !== index,
     id: `tabpanel-${index}`,
     "aria-labelledby": `tab-${index}`,
     ...other,
-    children:
-      value === index &&
-      /*#__PURE__*/ _jsx(Box, {
-        sx: {
-          p: 3,
-        },
-        children: children,
-      }),
+    children: value === index && /*#__PURE__*/_jsx(Box, {
+      sx: {
+        p: 3
+      },
+      children: children
+    })
   });
 }
 
 TabPanel.propTypes = {
   children: PropTypes.node.isRequired,
   index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired
 };
 
-const SitoMUITabView = (props) => {
+const SitoMUITabView = props => {
   const {
     id,
     name,
@@ -48,85 +51,68 @@ const SitoMUITabView = (props) => {
     tabsSx,
     contentSx,
     style,
+    icons,
+    iconsPosition
   } = props;
   const [localTab, setLocalTab] = useState(0);
 
   const localOnChange = (event, newTab) => setLocalTab(newTab);
 
-  return /*#__PURE__*/ _jsxs(Box, {
+  return /*#__PURE__*/_jsxs(Box, {
     id: id,
     name: name,
     className: className,
     sx: {
       width: "100%",
-      ...sx,
+      ...sx
     },
-    style: { ...style },
-    children: [
-      tabsAtTop &&
-        /*#__PURE__*/ _jsx(Box, {
-          sx: {
-            borderBottom: 1,
-            borderColor: "divider",
-            ...tabsContainerSx,
-          },
-          children: /*#__PURE__*/ _jsx(Tabs, {
-            textColor: color,
-            indicatorColor: color,
-            value: value || localTab,
-            onChange: onChange || localOnChange,
-            sx: { ...tabsSx },
-            children: tabs.map((item, i) =>
-              /*#__PURE__*/ _jsx(
-                Tab,
-                {
-                  component: "a",
-                  href: `#${item}`,
-                  label: item,
-                },
-                item
-              )
-            ),
-          }),
-        }),
-      content.map((item, i) =>
-        /*#__PURE__*/ _jsx(
-          TabPanel,
-          {
-            value: value || localTab,
-            index: i,
-            children: item,
-          },
-          `tc${i}`
-        )
-      ),
-      tabsAtBottom &&
-        /*#__PURE__*/ _jsx(Box, {
-          sx: {
-            borderBottom: 1,
-            borderColor: "divider",
-            ...tabsContainerSx,
-          },
-          children: /*#__PURE__*/ _jsx(Tabs, {
-            textColor: "primary",
-            indicatorColor: "primary",
-            value: value,
-            onChange: onChange,
-            sx: { ...contentSx },
-            children: tabs.map((item, i) =>
-              /*#__PURE__*/ _jsx(
-                Tab,
-                {
-                  component: "a",
-                  href: `#${item}`,
-                  label: item,
-                },
-                item
-              )
-            ),
-          }),
-        }),
-    ],
+    style: { ...style
+    },
+    children: [tabsAtTop && /*#__PURE__*/_jsx(Box, {
+      sx: {
+        borderBottom: 1,
+        borderColor: "divider",
+        ...tabsContainerSx
+      },
+      children: /*#__PURE__*/_jsx(Tabs, {
+        textColor: color,
+        indicatorColor: color,
+        value: value || localTab,
+        onChange: onChange || localOnChange,
+        sx: { ...tabsSx
+        },
+        children: tabs && tabs.map((item, i) => /*#__PURE__*/_jsx(Tab, {
+          label: item,
+          component: "a",
+          icon: icons[i] || "",
+          href: `#${item}`,
+          iconPosition: iconsPosition[i] || "start"
+        }, item))
+      })
+    }), content && content.map((item, i) => /*#__PURE__*/_jsx(TabPanel, {
+      value: value || localTab,
+      index: i,
+      children: item
+    }, `tc${i}`)), tabsAtBottom && /*#__PURE__*/_jsx(Box, {
+      sx: {
+        borderBottom: 1,
+        borderColor: "divider",
+        ...tabsContainerSx
+      },
+      children: /*#__PURE__*/_jsx(Tabs, {
+        textColor: "primary",
+        indicatorColor: "primary",
+        value: value,
+        onChange: onChange,
+        sx: { ...contentSx
+        },
+        children: tabs.map((item, i) => /*#__PURE__*/_jsx(Tab, {
+          component: "a",
+          href: `#${item}`,
+          label: item
+        }, item))
+      })
+    })]
   });
 };
 
@@ -144,6 +130,8 @@ SitoMUITabView.defaultProps = {
   tabsSx: {},
   contentSx: {},
   style: {},
+  icon: [],
+  iconsPosition: []
 };
 SitoMUITabView.propTypes = {
   id: PropTypes.string,
@@ -156,40 +144,12 @@ SitoMUITabView.propTypes = {
   tabs: PropTypes.arrayOf(PropTypes.string).isRequired,
   value: PropTypes.number,
   onChange: PropTypes.func,
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
-    ),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-  tabsContainerSx: PropTypes.oneOfType([
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
-    ),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-  tabsSx: PropTypes.oneOfType([
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
-    ),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-  contentSx: PropTypes.oneOfType([
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
-    ),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-  style: PropTypes.oneOfType([
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
-    ),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
+  icon: PropTypes.arrayOf(PropTypes.node),
+  iconsPosition: PropTypes.arrayOf(PropTypes.oneOf(["start", "end", "bottom", "top"])),
+  sx: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])), PropTypes.func, PropTypes.object]),
+  tabsContainerSx: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])), PropTypes.func, PropTypes.object]),
+  tabsSx: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])), PropTypes.func, PropTypes.object]),
+  contentSx: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])), PropTypes.func, PropTypes.object]),
+  style: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])), PropTypes.func, PropTypes.object])
 };
 export default SitoMUITabView;
